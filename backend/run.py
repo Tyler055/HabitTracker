d38d9372c3a129f8062958cac035a48824b7861d
Tyler055 <darkneuralai@gmail.com>
2025-03-29 15:57:40 -0500

    0

--- backend/run.py
+++ backend/run.py
@@ -1,19 +1,12 @@
-# run.py
 import os
-from app import create_app
+from app.app import create_app
 
-# Get environment mode from .env, default to 'development'
-env_mode = os.getenv('APP_MODE', 'development')
+# Get environment mode from .env, default to development
+env_mode = os.getenv('FLASK_ENV', 'development')
 
-# Create the app with the appropriate environment configuration
 app = create_app(env_mode)
 
 if __name__ == "__main__":
-    # Set debug mode based on the environment
     debug_mode = env_mode == 'development'
-    
-    # Get the port from environment variables, default to 5000
     port = int(os.getenv('PORT', 5000))
-    
-    # Run the app
-    app.run(port=port, debug=debug_mode)
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
