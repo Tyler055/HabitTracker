588e509591ac3fab084b1853dde308e3f24737a0
Tyler055 <darkneuralai@gmail.com>
2025-03-29 18:08:34 -0500

    0

--- frontend-0/src/pages/Profile.js
+++
@@ -1,2 +0,0 @@
-<h2>Profile Page</h2>;
-export default Profile;
---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,8 @@
+// src/pages/Profile.js
+import React from 'react';
+
+const Profile = () => {
+  return <h2>Profile Page</h2>;
+};
+
+export default Profile;
